44f1752c1d1314a9752e97f07b7fd343b1083d0a
Mike Feldman <mike@mikefeldman.me>
2025-07-03 23:48:46 -0400

    Initial commit

---
+++ visit.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+URL = "dc311eta.streamlit.app"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get(URL)
+
+print("Visited Streamlit app successfully.")
+
+driver.quit()
