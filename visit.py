62f431999aab4119919279f47fe79d7aae358146
Mike Feldman <mike@mikefeldman.me>
2025-07-04 00:06:20 -0400

    Debug action

--- visit.py
+++ visit.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 URL = "https://dc311eta.streamlit.app/"
 
-options = webdriver.ChromeOptions()
+options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.binary_location = '/usr/bin/chromium-browser'
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(options=options)
 driver.get(URL)
 
 print("Visited Streamlit app successfully.")
-
 driver.quit()
